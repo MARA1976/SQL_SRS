8f913bd35bf43c0b8038fb6def8344a4f5534194
ahlem <oueslatiahlem76@gmail.com>
2025-03-29 12:23:32 +0100

    app.py

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
 import streamlit as st
+import duckdb as db
 
 st.write("Hello world!")
+with st.sidebar:
+    theme = st.selectbox(
+        "what would you like to review?",
+        ("cross_Joins", "GroupBy", "Windows Functions"),
+        index=None,
+        placeholder="Select a theme..."
+    )
+    st.write("You selected:", theme)
